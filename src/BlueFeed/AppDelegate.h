1e34335619bd6326ee6f9da1b45a74968c24bb54
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2015-01-06 22:14:52 -0500

    Added dapp project and blank new project

---
+++ src/BlueFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlueFeed
+//
+//  Created by Chris Rittelmeyer on 1/6/15.
+//  Copyright (c) 2015 Chris Rittelmeyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
